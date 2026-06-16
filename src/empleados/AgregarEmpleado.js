838439e5c3a32d5241048d5a3d9712513951d921
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-16 17:11:22 -0600

    create works

--- src/empleados/AgregarEmpleado.js
+++ src/empleados/AgregarEmpleado.js
@@ -1,28 +1,58 @@
 import React from 'react'
+import { useState } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 export default function AgregarEmpleado() {
+    let navegacion = useNavigate();
+
+    const [empleado, setEmpleado] =useState({
+        nombre: '',
+        apellido: '',
+        departamento: '',
+        sueldo: ''
+    });
+
+    const{nombre, apellido, departamento, sueldo} = empleado;
+    const onInputChange = (e) => {
+        //spread operator para copiar el objeto empleado y luego actualizar el campo que se esta modificando
+        setEmpleado({...empleado, [e.target.name]: e.target.value});
+    }
+
+    const   onSubmit = async (e) => {
+        e.preventDefault();
+        const urlBase = 'http://localhost:8080/rh-app/empleados';
+        await axios.post(urlBase, empleado);
+        //redirigimos a la pagina de listado de empleados
+        navegacion("/");
+    }
+
   return (
     <div className="container">
         <div className="container text-center" style={{ margin: '30px' }}>
             <h3>Agregar Empleado</h3>
         </div>
 
-        <form>
+        <form onSubmit={(e) => onSubmit(e)}>
             <div className="mb-3">
                 <label htmlFor="nombre" className="form-label">Nombre</label>
-                <input type="text" className="form-control" id="nombre" name="nombre" placeholder="Ingrese el nombre del empleado" required={true}/>
+                <input type="text" className="form-control" id="nombre" name="nombre" placeholder="Ingrese el nombre del empleado"
+                value={nombre} onChange={(e) => onInputChange(e)} required={true}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="apellido" className="form-label">Apellido</label>
-                <input type="text" className="form-control" id="apellido" name="apellido" placeholder="Ingrese el apellido del empleado" required={true}/>
+                <input type="text" className="form-control" id="apellido" name="apellido" placeholder="Ingrese el apellido del empleado" 
+                 value={apellido} onChange={(e) => onInputChange(e)} required={true}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="departamento" className="form-label">Departamento</label>
-                <input type="text" className="form-control" id="departamento" name="departamento" placeholder="Ingrese el departamento del empleado" required={true}/>
+                <input type="text" className="form-control" id="departamento" name="departamento" placeholder="Ingrese el departamento del empleado" 
+                value={departamento} onChange={(e) => onInputChange(e)} required={true}/>
             </div>
             <div className="mb-3">
                 <label htmlFor="sueldo" className="form-label">Sueldo</label>
-                <input type="number" step="any" className="form-control" id="sueldo" name="sueldo" placeholder="Ingrese el sueldo del empleado" required={true}/>
+                <input type="number" step="any" className="form-control" id="sueldo" name="sueldo" placeholder="Ingrese el sueldo del empleado" 
+                value={sueldo} onChange={(e) => onInputChange(e)} required={true}/>
             </div>
             <div className="text-center">
                 <button type="submit" className="btn btn-warning btn-sm me-3">Agregar</button>
