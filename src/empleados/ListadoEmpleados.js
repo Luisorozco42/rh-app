e74c371b67b4fa0bc01937a0a46a7d9406fc3a7d
Luisorozco42 <lorozcolazo@gmail.com>
2026-06-17 04:30:08 -0600

    updating an employee works

--- src/empleados/ListadoEmpleados.js
+++ src/empleados/ListadoEmpleados.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
 import { NumericFormat } from 'react-number-format';
+import { Link } from 'react-router-dom';
 
 export default function ListadoEmpleados() {
 
@@ -30,6 +31,7 @@ export default function ListadoEmpleados() {
                         <th scope="col">Nombre</th>
                         <th scope="col">Departamento</th>
                         <th scope="col">Sueldo</th>
+                        <th scope="col"></th>
                     </tr>
                 </thead>
                 <tbody>
@@ -42,6 +44,12 @@ export default function ListadoEmpleados() {
                                 <td>{empleado.departamento}</td>
                                 <td> <NumericFormat value={empleado.sueldo} displayType={'text'} thousandSeparator={","}
                                 prefix={'$'} decimalScale={2} fixedDecimalScale/> </td>
+                                <td className="text-center">
+                                    <div>
+                                        <Link to={`/editar/${empleado.idEmpleado}`} className="btn btn-warning btn-sm me-3">Editar</Link>
+                                        <button className="btn btn-danger btn-sm">Eliminar</button>
+                                    </div>
+                                </td>
                             </tr>
                         ))
                     }
